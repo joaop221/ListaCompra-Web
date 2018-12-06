24ba169615cf37d594c822bd1321c0a420e7c44c
João Pedro Barboza Garcia - JGA <joao.garcia@iteris.com.br>
2018-12-06 21:29:36 -0200

    Erro nos guards

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import * as pages from './components/pages';
-import * as guards from './guards';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
